5b7468078596ea0230aabb12e6abb8daea5e7f7a
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-08-16 13:21:38 -0400

    initial commit

---
+++ fitplusplus/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  const content = "Hello, World!";
+
+  return (
+    <div>
+      <h1>{content}</h1>
+    </div>
+  );
+};
+
+export default App;
