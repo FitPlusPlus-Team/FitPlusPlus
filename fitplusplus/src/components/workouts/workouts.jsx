a904d329c0e4fb0720e5c4fbd39fcd4df9f5cc21
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2021-08-25 16:27:01 -0400

    started workouts tab

--- fitplusplus/src/components/workouts/workouts.jsx
+++ fitplusplus/src/components/workouts/workouts.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Navbar from "../Navbar.jsx"
+import "./style.css";
 
 const Workouts = () => {
   return (
-    <div>
+    <div id="main">
+      <Navbar />
       <h1>This is the Exercises Page.</h1>
     </div>
   );
