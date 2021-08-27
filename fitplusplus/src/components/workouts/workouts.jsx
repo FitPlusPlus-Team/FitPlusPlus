e2e0617320f5a8742ef3ae452f9f3ee2a007bf2a
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2021-08-27 09:17:06 -0400

    more work on exercises page

--- fitplusplus/src/components/workouts/workouts.jsx
+++ fitplusplus/src/components/workouts/workouts.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import Navbar from "../Navbar.jsx"
-import "./style.css";
+import "./style.scss";
 
 const Workouts = () => {
   return (
     <div id="workouts">
       <Navbar />
-      <h1>This is the Exercises Page.</h1>
+      <NewWorkouts />
     </div>
   );
 };
+
+const SectionHeader = (props) => {
+    return (
+        <div className="section-header">
+            <h1>{props.title}</h1>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+const NewWorkouts = () => {
+    return (
+        <SectionHeader title="New workouts" description="find the newest workouts made by us!"/>
+    ) 
+}
 
 export default Workouts;
