6b86449b9a1e409a0029638e2689ff824d2c7df2
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2021-08-27 09:40:59 -0400

    workout cards

--- fitplusplus/src/components/workouts/workouts.jsx
+++ fitplusplus/src/components/workouts/workouts.jsx
@@ -22,8 +22,27 @@ const SectionHeader = (props) => {
 
 const NewWorkouts = () => {
     return (
-        <SectionHeader title="New workouts" description="find the newest workouts made by us!"/>
+        <section>
+            <SectionHeader title="New workouts" description="find the newest workouts made by us!"/>
+            <div className="horizontal-scroll">
+                <WorkoutCards title="Thing" time="30 min" />
+                <WorkoutCards title="Thing" time="30 min" />
+                <WorkoutCards title="Thing" time="30 min" />
+                <WorkoutCards title="Thing" time="30 min" />
+            </div>
+        </section>
     ) 
+}
+
+const WorkoutCards = (props) => {
+    return (
+        <div className="workout-card">
+            <div className="card-description">
+                <h1>{props.title}</h1>
+                <p>{props.time}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Workouts;
