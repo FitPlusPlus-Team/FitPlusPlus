8c6c959fe2a823d09ce669bc5ac858ee44961fbf
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2021-08-29 09:47:30 -0400

    workout data as object

--- fitplusplus/src/components/workout/workout.jsx
+++ fitplusplus/src/components/workout/workout.jsx
@@ -3,7 +3,38 @@ import { Link } from "react-router-dom";
 import "./style.scss";
 
 
+let placeholderData = {
+    name: "Placeholder Workout",
+    background:"linear-gradient(45deg, #9A77D2, #5B78DD)",
+    actions: [
+        {
+            type: "timer",
+            duration: "30",
+            label: "timer 1"
+        },
+        {
+            type: "timer",
+            duration: "20",
+            label: "timer 2"
+        },
+        {
+            type: "timer",
+            duration: "10",
+            label: "timer 3"
+        }
+    ]
+}
+
 class Workout extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            action: 0,
+            isPaused: false,
+            workout: placeholderData,
+        }
+    }
+
     render(){
         return (
             <div id="workout">
