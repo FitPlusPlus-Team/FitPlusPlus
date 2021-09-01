458f81ad06ccbba873075db09e12607aa88f8adf
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-09-01 14:31:18 -0400

    added foodlist.jsx

--- fitplusplus/src/components/food/food.jsx
+++ fitplusplus/src/components/food/food.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../Navbar";
+import { foodList } from "./foodlist";
 import "./style.scss";
 
 const Food = () => {
   return (
     <div id="food-page">
       <Navbar />
+      <div className="foodContainer">
+        <div className="foodSelection"></div>
+        <div className="foodDisplay"></div>
+      </div>
     </div>
   );
 };
---
+++ fitplusplus/src/components/food/foodlist.jsx
@@ -0,0 +1,25 @@
+export const foodList = {
+  meat: {
+    category: "Meat",
+    chickenBreast: {
+      foodName: "Chicken Breast",
+      information: "Text...",
+    },
+  },
+
+  fruit: {
+    category: "Fruit",
+    Apple: {
+      foodName: "Apple",
+      information: "Text...",
+    },
+  },
+
+  vegetable: {
+    category: "Vegetable",
+    Spinach: {
+      foodName: "Spinach",
+      information: "Text...",
+    },
+  },
+};
