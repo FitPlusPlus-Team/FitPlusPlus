ba12a94bf544318e730da23b6857eb18030c0af6
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2021-09-11 11:42:22 -0400

    one workout

--- fitplusplus/src/workoutData.js
+++ fitplusplus/src/workoutData.js
@@ -1,42 +1,58 @@
 
 export let workoutData = [
     {
-        name: "Placeholder Workout",
+        name: "30's",
         id: "placeholder-workout",
         background: "linear-gradient(45deg, #9A77D2, #5B78DD)",
-        time: "30 min",
+        time: "15 min",
         actions: [
             {
                 type: "start",
                 name: "Start",
             },
             {
-                type: "timer",
-                duration: 10_000,
-                name: "10 second run",
+                type: "counter",
+                duration: 1_000,
+                count: 30,
+                name: "30 Jumping Jacks",
             },
             {
-                type: "counter",
-                count: 20,
-                duration: 2_000,
-                name: "20 pushups",
+                type: "timer",
+                duration: 30_000,
+                name: "30 second High Knees",
+            },
+            {
+                type: "timer",
+                duration: 30_000,
+                name: "30 second Butt Kicks"
             },
             {
                 type: "text",
                 duration: 10_000,
-                name: "10 second rest"
+                name: "10 second rest",
+            },
+            {
+                type: "counter",
+                duration: 5_000,
+                name: "30 Squats"
+            },
+            {
+                type: "text",
+                duration: 3_000,
+                name: "5 second rest",
             },
             {
                 type: "timer",
-                duration: 40_000,
-                name: "40 second plank",
+                duration: 30_000,
+                name: "30 second Mountain Climbers"
             },
             {
                 type: "finish",
                 name: "All done",
             },
         ],
-}
+    },
+
 ]
 
 
